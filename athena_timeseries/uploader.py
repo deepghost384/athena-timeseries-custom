7eea9c997b3aee0e57b7e814b2a0cba188bb0254
None <None>
2024-07-06 17:55:48 +0900

    Add mode selection to upload function
    Introduce 'mode' parameter to upload function, allowing users to choose between 'append', 'overwrite', and 'overwrite_partitions'. Update awswrangler.s3.to_parquet call and add mode info to console output. Provides flexibility for different data update strategies in S3.

--- athena_timeseries/uploader.py
+++ athena_timeseries/uploader.py
@@ -1,4 +1,4 @@
-from typing import Optional, Dict
+from typing import Optional, Dict, Literal
 import pandas as pd
 import awswrangler
 from rich.console import Console
@@ -14,32 +14,27 @@ def upload(
     table_name: str,
     df: pd.DataFrame,
     dtype: Optional[Dict[str, str]] = None,
+    mode: Literal['append', 'overwrite', 'overwrite_partitions'] = 'overwrite_partitions'
 ):
     _dtype = {
         "partition_dt": "date",
         "dt": "timestamp",
         "symbol": "string",
     }
-
     for (key, value) in _dtype.items():
         if key not in df.columns:
             raise ValueError(f"Column {key} must be given with dtype {value}")
-
     if dtype is not None:
         for k, v in dtype.items():
             _dtype[k] = v
-
     console = Console()
-
     with Progress(
         SpinnerColumn(),
         TextColumn("[progress.description]{task.description}"),
         console=console,
     ) as progress:
         task = progress.add_task(description="Uploading data...", total=None)
-
         start_time = time.time()
-
         result = awswrangler.s3.to_parquet(
             df=df,
             partition_cols=["partition_dt", "symbol"],
@@ -48,24 +43,19 @@ def upload(
             table=table_name,
             path=f"{s3_path}/{table_name}",
             boto3_session=boto3_session,
-            mode="overwrite_partitions",
+            mode=mode,  # ここで選択されたmodeを使用
             concurrent_partitioning=True,
             dtype=_dtype,
             compression="snappy",  # Snappy圧縮を適用
         )
-
         end_time = time.time()
-
         progress.update(task, completed=True, description="Upload completed!")
-
     # テーブルサイズの計算
     table_size = df.memory_usage(deep=True).sum()
     table_size_mb = table_size / (1024 * 1024)  # バイトからメガバイトに変換
-
     # アップロード時間の計算
     upload_time = end_time - start_time
-
     console.print(f"Table size: {table_size_mb:.2f} MB")
     console.print(f"Upload time: {upload_time:.2f} seconds")
-
+    console.print(f"Upload mode: {mode}")
     return result
